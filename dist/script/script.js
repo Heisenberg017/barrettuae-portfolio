a62e4c93eb9cefc6250dcc95abfdfb426329b6f0
Heisenberg017 <suyash9211@gmail.com>
2019-03-01 17:20:33 +0530

    Multiple Sections And MQ Added

--- dist/script/script.js
+++ dist/script/script.js
@@ -1,6 +1,6 @@
 //Navigation Bar JS
-(function ($) { 
-    $(function () { 
+(function ($) {
+    $(function () {
         $('nav ul li a:not(:only-child)').click(function (e) {
             $(this).siblings('.nav-dropdown').toggle();
             $('.nav-dropdown').not($(this).siblings()).hide();
@@ -16,4 +16,78 @@
             this.classList.toggle('active');
         });
     });
-})($); 
+})($);
+
+//Scroll Animation
+ScrollReveal().reveal('#about', {
+    duration: 2000,
+    interval: 200,
+    delay: 100,
+    reset: true,
+    origin: 'left',
+    scale: 0.85,
+});
+
+ScrollReveal().reveal('#service', {
+    duration: 2000,
+    interval: 200,
+    delay: 100,
+    reset: true,
+    origin: 'left',
+    scale: 0.85,
+});
+
+ScrollReveal().reveal('#feature', {
+    duration: 2000,
+    interval: 200,
+    delay: 100,
+    reset: true,
+    origin: 'left',
+    scale: 0.85,
+});
+
+ScrollReveal().reveal('#portfolio', {
+    duration: 2000,
+    interval: 200,
+    delay: 100,
+    reset: true,
+    origin: 'left',
+    scale: 0.85,
+});
+
+ScrollReveal().reveal('#team', {
+    duration: 2000,
+    interval: 200,
+    delay: 100,
+    reset: true,
+    origin: 'left',
+    scale: 0.85,
+});
+
+ScrollReveal().reveal('#testimonial', {
+    duration: 2000,
+    interval: 200,
+    delay: 100,
+    reset: true,
+    origin: 'left',
+    scale: 0.85,
+});
+
+ScrollReveal().reveal('#contact', {
+    duration: 2000,
+    interval: 200,
+    delay: 100,
+    reset: true,
+    origin: 'left',
+    scale: 0.85,
+});
+
+
+ScrollReveal().reveal('#footer', {
+    duration: 2000,
+    interval: 200,
+    delay: 100,
+    reset: true,
+    origin: 'left',
+    scale: 0.85,
+});
